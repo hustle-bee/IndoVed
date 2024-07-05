977735d5e9346a3c30bc693b3ceae6e399c436ab
anushka sontakke <anushkasontakke123@gmail.com>
2024-07-05 17:10:14 +0530

    Added primary image in the shop page.

---
+++ js/shop.js
@@ -0,0 +1,22 @@
+const primaryImage = document.getElementsByClassName("Primary-image");
+
+const category = document.getElementsByClassName("catagory-name");
+
+const price = document.getElementsByClassName("product-price");
+
+const collection = document.getElementsByClassName("product-name");
+
+for (let i = 0; i < myProducts.length; i++) {
+  let imageObj = primaryImage[i];
+  imageObj.src = myProducts[i].primaryImage;
+
+  let categoryObj = category[i];
+  categoryObj.innerText = myProducts[i].category;
+
+  let priceObj = price[i];
+  priceObj.innerText = myProducts[i].price;
+
+  let collectionObj = collection[i];
+  console.log(collectionObj);
+  collectionObj.innerText = myProducts[i].collection;
+}
