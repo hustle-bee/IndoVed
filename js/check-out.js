89de2e8222c03262a6c8f228f7dbc95b096b0ccf
rohithustlebee15 <rohit.gundeti@hustlebee.net>
2024-09-10 14:03:16 +0530

    implemented order placed message UI

--- js/check-out.js
+++ js/check-out.js
@@ -30,3 +30,22 @@ if (orderTableProduct) {
   });
   document.getElementById("total-price").textContent = `Total RS: ${total}`;
 }
+
+//pop up
+const openPopupBtn = document.getElementById("openPopupBtn");
+const popup = document.getElementById("popup");
+const closePopupBtn = document.getElementById("closePopupBtn");
+
+openPopupBtn.addEventListener("click", () => {
+  popup.style.display = "flex";
+});
+
+closePopupBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === popup) {
+    popup.style.display = "none";
+  }
+});
