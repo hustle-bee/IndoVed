985274b74201e9f61b2921affe0ae7f75d47fdf0
rohithustlebee15 <rohit.gundeti@hustlebee.net>
2024-09-13 12:33:08 +0530

    clearing all components on product search

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,11 +1,23 @@
 function searchProducts() {
   const query = document.getElementById("searchbar-input").value;
-  var searchStr = query.toLowerCase();
-  const result = myProducts.filter((product) => {
-    return (
-      product.category.toLowerCase().includes(searchStr) ||
-      product.collection.toLowerCase().includes(searchStr)
-    );
-  });
-  console.log("result: ", result);
+  if (query) {
+    document.getElementById("sections").style.display = "none";
+    var searchStr = query.toLowerCase();
+    const result = myProducts.filter((product) => {
+      return (
+        product.category.toLowerCase().includes(searchStr) ||
+        product.collection.toLowerCase().includes(searchStr)
+      );
+    });
+    console.log("result: ", result);
+  } else {
+    document.getElementById("sections").style.display = "block";
+  }
 }
+
+const inputElement = document.getElementById("searchbar-input");
+
+inputElement.addEventListener("input", function () {
+  const inputValue = inputElement.value;
+  if (!inputValue) searchProducts();
+});
